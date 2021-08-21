c74fa6fd4e4235bbe2ad708909db9ab96a902fa4
yhoon5171 <65946205+yhoon5171@users.noreply.github.com>
2021-08-21 17:17:31 +0900

    Update 공주구하기(조세퍼스).cpp

--- 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/공주구하기(조세퍼스).cpp
+++ 2. 정렬 & 이분탐색(결정알고리즘) & 투포인트 알고리즘 & 스택/공주구하기(조세퍼스).cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+int main()
+{
+    int i, n, k, pos = 0;
+    int cnt = 0;
+    scanf("%d %d", &n, &k); 
+    vector<int> check(n);
+
+    for (i = 0; i < n - 1; i++){
+        cnt = 0;
+        while (cnt != k){
+            if (check[pos] == 0) cnt++;
+            
+            if (pos + 1 == n) pos = 0;
+            else pos++;
+        }
+        if (pos == 0) check[n - 1] = 1;
+        else check[pos - 1] = 1;
+    }
+    for (i = 0; i < n; i++){
+        if (check[i] == 0){
+            printf("%d", i + 1);
+            break;
+        }
+    }
+    return 0;
+}
